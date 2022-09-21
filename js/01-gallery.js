7c7df95c5459e52e43a086161d3e838d21bd483d
Maryna-Dr <drapata.mb@gmail.com>
2022-09-22 01:41:19 +0300

    Task 1 completed

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const markupGalleryItem = galleryItems.map(it => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${it.original}">
+    <img
+      class="gallery__image"
+      src="${it.preview}"
+      data-source="${it.original}"
+      alt="${it.description}"
+    />
+  </a>
+</div>`; 
+}).join('');
+
+gallery.insertAdjacentHTML('afterbegin', markupGalleryItem);
+
+gallery.addEventListener('click', onGalleryItemClick);
+
+let modalCreate = null;
+
+function onGalleryItemClick(e) {
+    if (e.target === e.currentTarget) {
+        return;
+    };
+
+    e.preventDefault();
+
+    const modal = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" heigth="600">
+    `,
+    
+    {
+        onClose: () => {
+            window.removeEventListener('keydown', onWindowClickEscape)
+            },
+        onShow: () => {
+            window.addEventListener('keydown', onWindowClickEscape)
+        },
+        },
+    );
+
+    modalCreate = modal;
+    modal.show();     
+};
+
+function onWindowClickEscape(e) {
+    if (e.code === 'Escape') {
+        modalCreate.close();
+    };
+
+    console.log(e.code);
+};
